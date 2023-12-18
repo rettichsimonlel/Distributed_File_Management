7a402e2dbe10123501ca47a53eb0273dd0efc9e8
rettichsimonlel <retter190085@sr.htlweiz.at>
2023-12-18 16:38:51 +0100

    test socket

--- algo.py
+++ algo.py
@@ -1,5 +1,6 @@
-from datetime import datetime
+from socke import _socket
 
+from datetime import datetime
 from threading import Thread
 from time import sleep
 
@@ -9,6 +10,7 @@ class Request:
         self.addr     = addr
         self.payload  = payload
         self.datetime = datetime
+        self.accept   = False
 
 class Algo:
     def __init__(self):
@@ -16,21 +18,24 @@ class Algo:
         self.request_list: list[Request] = []
         self.duing                       = False
         self.ipaddr                      = "127.0.0.1"
+        self.ipport                      = "7450"
         self.keep_alive_thread           = Thread(target=self.multicast_keep_alive)
 
-    def send_multicast(self, payload):
+    async def __send_multicast(self, payload) -> list[Request]:
+        requests = []
         for i in self.others:
-            self.send_request(i, payload)
-
+            request = await self.send_request(i, payload)
+        return requests
+                
     def not_responding(self, other):
-        self.send_multicast(self, f"{other} not responding")
+        self.__send_multicast(self, f"{other} not responding")
 
     def request_ressource(self, req: Request) -> None:
         self.request_list.append(req)
         self.request_list.sort(key=lambda x: x.datetime)
-        return self.return_acceptance()
+        return self. return_acceptance()
         
-    def send_request(self, other, payload):
+    async def send_request(self, other, payload) -> Request:
         # other = ...
         # payload = "request_ressource"
         # Socket
@@ -39,15 +44,14 @@ class Algo:
             addr=other,
             payload=payload,
             datetime=datetime.now())
-        ...
-        return socket(request)
+        socke = _socket()
+        socke.send_request(other, self.ipport, payload)
+        # return socket(request)
 
     def multicast_keep_alive(self):
         while True:
-            for i in self.others:
-                response = self.send_request(i, "keep_alive")
-                if response != True:
-                    self.not_responding(i)
+            if self.__send_multicast("keep_alive") == False:
+                ... # self.not_responding
             sleep(10)
 
     def answer_keep_alive(self):
@@ -64,14 +68,8 @@ class Algo:
         return not self.duing
 
     def get_acceptance(self) -> bool:
-        allowed = True
-        for i in self.others:
-            response = self.send_request(i, "return_acceptance")
-            if not response:
-                allowed = False
-        return allowed
+        return self.__send_multicast(self, "return_acceptance")
 
 algo = Algo()
-algo.add_others(["",])
-
-request = Request('test', '1', 'pay', datetime.now())
+algo.add_others(["172.31.182.123"])
+algo.send_request("172.31.182.123", "Weak minded")
---
+++ socke.py
@@ -0,0 +1,24 @@
+import socket
+
+
+class _socket:
+
+    def __init__(self, sock=None) -> None:
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def listen_(self):
+        self.sock.bind(('', 80))
+        self.sock.listen(10)
+        address = self.sock.accept()
+        return print('Connected with ', address)
+
+    def receive_request(self):
+        msg = self.sock.recv(1024)
+        msg.decode("utf-8")
+        return msg
+
+    def send_request(self, ip, port, msg):
+        self.sock.connect((ip, port))
+        self.sock.send(msg.encode("utf-8")[:1024])
+        self.sock.close()
