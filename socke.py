f16e7f311509a9bc37dc8095fc7ab75d89affe65
simonGoelles <goelle190028@sr.htlweiz.at>
2023-12-20 10:48:12 +0100

    try like this then

--- socke.py
+++ socke.py
@@ -2,27 +2,31 @@ import socket
 
 
 class _socket:
-
     def __init__(self, sock=None) -> None:
         if sock is None:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection = None
 
     def listen_(self):
         self.sock.bind(('', 80))
         self.sock.listen(10)
-        address = self.sock.accept()
-        return print('Connected with ', address)
+        self.connection, address = self.sock.accept()
+        return self.connection, address
 
     def receive_request(self):
-        msg = self.sock.recv(1024)
-        msg.decode("utf-8")
+        if self.connection is None:
+            raise ValueError("Connection is not established.")
+
+        msg = self.connection.recv(1024)
+        msg = msg.decode("utf-8")
         return msg
 
     def send_request(self, ip, port, msg) -> bool:
         try:
-            self.sock.connect((ip, port))
-            self.sock.send(msg.encode("utf-8")[:1024])
-            self.sock.close()
+            send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            send_socket.connect((ip, port))
+            send_socket.send(msg.encode("utf-8")[:1024])
+            send_socket.close()
             return True
         except Exception as e:
             print(f"Error sending message: {e}")
